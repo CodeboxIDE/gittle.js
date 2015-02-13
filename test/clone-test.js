16e5db10376271b29f4e3cbf5f0a055ce36ba0a3
Samy Pessé <samypesse@gmail.com>
2015-02-13 09:05:21 +0100

    Update from udnerscore to lodash
    Update pty.js

--- test/clone-test.js
+++ test/clone-test.js
@@ -11,27 +11,9 @@ exports.clones = {
         qtest(
             Q.nfcall(tmp.dir)
             .then(function(path) {
-                return Gittle.clone("https://SamyPesse@bitbucket.org/SamyPesse/node-js-sample.git", path);
+                return Gittle.clone("https://SamyPesse@github.com/CodeboxIDE/gittle.js.git", path);
             }),
             test
         );
-    },
-
-    // Clone https repo with invalid auth
-    httpsAuth: function(test) {
-        Q.nfcall(tmp.dir)
-        .then(function(path) {
-            return Gittle.clone("https://invalid@bitbucket.org/SamyPesse/node-js-sample2.git", path, {
-                username: "invalid",
-                password: "invalid"
-            });
-        })
-        .then(function(out) {
-            test.ok(false);
-        }, function(err) {
-            test.ok(true);
-        }).fin(function() {
-            test.done();
-        });
     }
 }
